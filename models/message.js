b85612041b5dd718360a491a42d2eac2a4ec2915
Philip Xu <philipxjm1@gmail.com>
2015-09-21 07:32:59 -0400

    initial commit

---
+++ models/message.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+    name: String,
+    email: String,
+    id: String
+});
+
+module.exports = mongoose.model('User', userSchema);
